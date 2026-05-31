50bcdcdc40c5ad8a671c6d69c3c4602ebd06d862
Tudi <ktudruj@hotmail.com>
2026-05-31 09:41:26 +0200

    chore(deploy): add support for loading multiple environment files in deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
+ENV_ARGS="--env-file .env"
+if [ -f .env.local ]; then
+    ENV_ARGS="$ENV_ARGS --env-file .env.local"
+fi
+
 echo "📥 Pulling changes..."
 git pull
 
 echo "🐳 Building containers (updating packages)..."
-docker compose build --no-cache api
+docker compose $ENV_ARGS build --no-cache api
 
 echo "🧬 Applying migrations..."
-docker compose run --rm api npm run migrate:deploy
+docker compose $ENV_ARGS run --rm api npm run migrate:deploy
 
 echo "🚀 Starting containers..."
-docker compose up -d --force-recreate --build
+docker compose $ENV_ARGS up -d --force-recreate --build
 
 echo "✅ Deploy done"
